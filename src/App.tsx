cc2fceb24317399eb860b0a95d3cc29f6acb0917
Alexandra77718 <sandra.davidson777@gmail.com>
2023-03-09 20:34:33 +0300

    create state BurgerButtons, component OpenedMenu, CloseIcon

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
 import React, {useState} from "react";
-import './App.css'
 import './App.module.scss';
 import Button from './components/Button';
 import { ButtonType } from './components/Button/Button';
 import UserName from './components/UserName';
 import Title from './components/Title';
-
 import Tabs from "./components/Tabs";
-// import { CloseIcon, OpenedMenu } from "./assets/icons";
+import BurgerButton from "./components/BurgerButton";
+import { CloseIcon, OpenedMenu } from "./assets/icons";
 
 
 
 const App = () => {
+    const [username, setUsername] = useState('');
+    const onChange = (value: string) => {
+        setUsername(value);
+    }
+
+
     return (
         <div className='App'>
-            <Button title={'Primary'} type={ButtonType.Primary} onClick={() =>{}} />
-            <Button title={'Secondary'} type={ButtonType.Secondary} onClick={() => {}} />
+            <input onChange={(event) => onChange(event.target.value)} />
+            <Button title={'Primary'} type={ButtonType.Primary} onClick={() => { }} />
+            <Button title={'Secondary'} type={ButtonType.Secondary} onClick={() => { }} />
             <Button title={'Error'} type={ButtonType.Error} onClick={() => { }} />
-            <UserName username={"Artem Malkin"} />
+            {/* <UserName username={username} /> username, onChange, value не подходит почему? */}
+            <UserName username="Artem Malkin" />
             <Title title={'Blog'} />
-            <Tabs /> 
+            <Tabs />
+            <BurgerButton />
+
+            
         </div>
         
-    )
+    );
 }
 
 export default App;
